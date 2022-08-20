b30470fec8b83ce9134b1a07b1eb66a7b17d7069
Dixit Vora <dixit.vohra@msgis.com>
2022-08-20 20:04:04 +0530

    week_5: updated the code as per the comments

--- Week-5/exercise_5.4/index.test.js
+++ Week-5/exercise_5.4/index.test.js
@@ -4,20 +4,23 @@ const MathOperations = require('./MathOperations');
 
 describe("Math operations testing", () => {
     test("Should Pass Addition test a = 20, b = 13 returns 33", () => {
-      let res = MathOperations.sum(20, 13);
+      const res = MathOperations.sum(20, 13);
       expect(res).toBe(33);
+      expect(res).not.toBe(20)
     });
   
     test("Should Pass Subtraction test a = 20, b = 13 returns 7", () => {
-      let res = MathOperations.diff(20, 13);
+      const res = MathOperations.diff(20, 13);
       expect(res).toBe(7);
+      expect(res).not.toBe(13)
     });
     test("Should Pass Subtraction test a = 13, b = 20 returns -7", () => {
-      let res = MathOperations.diff(13, 20);
+      const res = MathOperations.diff(13, 20);
       expect(res).toBe(-7);
+      expect(res).not.toBe(7)
     });
     test("Should Pass Addition test a = 20, b = 13 returns 260", () => {
-      let res = MathOperations.product(20, 13);
+      const res = MathOperations.product(20, 13);
       expect(res).toBe(260);
     });
   });
